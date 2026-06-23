0b0fa673d14806a92b34475da975785b3061e99e
huyy194 <huyy1942000@gmail.com>
2026-06-23 19:51:28 +0700

    Them chuc nang dang ky dang nhap quen mat khau dang xuat

--- QuanLyKhachSan/test_reset.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'config/database.php';
-$username = 'admin'; // wait, admin doesn't have a KhachHang record.
-$res = $conn->query("SELECT * FROM TaiKhoan WHERE TenDangNhap='admin'");
-print_r($res->fetch_assoc());
